9f49152a8bd8c7dedc55faaca929bf3f8a375ef9
nayurin <nayurin@outlook.com>
2020-10-26 22:22:48 +0800

    update: 新增chara/profile接口

---
+++ src/router/apis/chara/profile.js
@@ -0,0 +1,28 @@
+const commons = require(`${__dirname}/../../commons.js`);
+const { logger4router } = require(`${__dirname}/../../../log4js`);
+const queryData = require(__dirname + '/../../../utils/sqliteutil.js');
+
+async function respond (ctx, next) {
+  try {
+    const data = await commons.parse(ctx);
+    const profile = queryData.queryFromDatabase('select distinct * from unit_data left join unit_profile on unit_data.unit_id=unit_profile.unit_id where unit_data.unit_id=?', data.id)[0];
+    logger4router.debug('<charaProfile> query result:', profile);
+    ctx.status = 200;
+    ctx.body = {
+      result: 'succeed',
+      data: JSON.stringify(profile)
+    };
+    logger4router.debug('<charaProfile> respond:', ctx.status, ctx.body);
+  } catch (e) {
+    ctx.status = 400;
+    ctx.body = {
+      result: 'failed',
+      reason: JSON.stringify(e)
+    };
+    logger4router.error('<charaProfile> caught an error:', e);
+  }
+}
+
+module.exports = {
+  respond
+};
--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,13 @@ const router = new Router();
 const newissue = require(`${__dirname}/apis/newissue.js`);
 const gemCalc = require(`${__dirname}/apis/arena/gemCalc.js`);
 const charaStats = require(`${__dirname}/apis/chara/stats.js`);
+const charaProfile = require(`${__dirname}/apis/chara/profile.js`);
 
 // apply routes
 router.post('/api/newissue', newissue.respond);
 router.post('/api/arena/gemcalc', gemCalc.respond);
 router.post('/api/chara/stats', charaStats.respond);
+router.post('/api/chara/profile', charaProfile.respond);
 
 // export module
 module.exports = router;
