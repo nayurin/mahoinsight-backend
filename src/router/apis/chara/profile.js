3944ac49e5908e16d2ccac6ecb0fa5994b78ea70
nayurin <nayurin@outlook.com>
2020-12-24 14:21:39 +0800

    add: non-params response

--- src/router/apis/chara/profile.js
+++ src/router/apis/chara/profile.js
@@ -5,21 +5,32 @@ const queryData = require(__dirname + '/../../../utils/sqliteutil.js');
 async function respond (ctx, next) {
   try {
     const data = qs.parse(ctx.request.url.split('?')[1])
-    const profile = queryData.queryFromDatabase('select distinct * from unit_data left join unit_profile on unit_data.unit_id=unit_profile.unit_id where unit_data.unit_id=?', `${data.baseid}01`)[0];
-    logger4router.debug('<charaProfile> query result:', profile);
-    if (profile) {
+    if (!data) {
+      const unitList = queryData.queryFromDatabase('select * from unit_profile').map(el => el.unit_id);
+      logger4router.debug('<charaProfile> query result:', unitList);
       ctx.status = 200;
       ctx.body = {
         result: 'succeed',
-        data: profile
+        data: unitList
       };
       logger4router.debug('<charaProfile> respond:', ctx.status, ctx.body);
     } else {
-      ctx.status = 400;
-      ctx.body = {
-        result: 'failed',
-        reason: '无法获取此ID的角色信息'
-      };
+      const profile = queryData.queryFromDatabase('select distinct * from unit_data left join unit_profile on unit_data.unit_id=unit_profile.unit_id where unit_data.unit_id=?', `${data.baseid}01`)[0];
+      logger4router.debug('<charaProfile> query result:', profile);
+      if (profile) {
+        ctx.status = 200;
+        ctx.body = {
+          result: 'succeed',
+          data: profile
+        };
+        logger4router.debug('<charaProfile> respond:', ctx.status, ctx.body);
+      } else {
+        ctx.status = 400;
+        ctx.body = {
+          result: 'failed',
+          reason: '无法获取此ID的角色信息'
+        };
+      }
     }
   } catch (e) {
     ctx.status = 400;
